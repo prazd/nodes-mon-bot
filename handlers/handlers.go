19d4d864618ccb2b0a814532de286be37b1caa8b
prazd <alexokda@list.ru>
2019-03-06 03:18:28 +0300

    ref

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,8 +5,6 @@ import (
 	"time"
 )
 
-type Result bool
-
 func NodesAlive(address string, port int, c chan bool) {
 	ps := portscanner.NewPortScanner(address, 2*time.Second, 5)
 	c <- ps.IsOpen(port)
